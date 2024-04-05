b9c217b78189443b51c856d66b2f5b8ddb16c6cb
Kevin Hu <kevinhu229@gmail.com>
2024-04-05 12:21:46 -0700

    initial commit

---
+++ Assets/KippsAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KippsAI : MonoBehaviour
+{
+    public float mDecayRate = 100.0f;
+    public float mInterestVal = 1000.0f;
+    public float mGrowthRate = 50.0f; 
+    public float mJumpCooldown = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
